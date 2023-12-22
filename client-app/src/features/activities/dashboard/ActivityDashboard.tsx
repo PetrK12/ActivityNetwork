a8f9d10c80d13fe61b4655756f11215ac201d5ba
Petrk12 <petrkutil123@gmail.com>
2023-12-22 23:29:10 +0100

    client activity CRUD implementation, backend connection

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,17 +14,19 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submiting: boolean;
 }
 
 export default function ActivityDasboard({activities, selectedActivity,
         selectActivity, cancelSelectActivity, editMode, openForm, closeForm,
-        createOrEdit, deleteActivity }: Props) {
+        createOrEdit, deleteActivity, submiting }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} 
                 selectActivity={selectActivity}
-                deleteActivity={deleteActivity}/>
+                deleteActivity={deleteActivity}
+                submiting={submiting}/>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -33,7 +35,10 @@ export default function ActivityDasboard({activities, selectedActivity,
                     openForm={openForm}/>
                 }
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>
+                    <ActivityForm closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submiting={submiting}/>
                 }
             </Grid.Column>
         </Grid>
