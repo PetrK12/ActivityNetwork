775d56b2c996e8a695a332ccba6fe8ab77e740a8
Petrk12 <petrkutil123@gmail.com>
2023-12-22 17:35:45 +0100

    CQRS implementation using mediator

--- API/Controllers/ActivityController.cs
+++ API/Controllers/ActivityController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistance;
@@ -8,23 +10,38 @@ namespace API.Controllers
 {
 	public class ActivityController : BaseApiController
 	{
-		private readonly DataContext _context;
-
-		public ActivityController(DataContext context)
-		{
-			_context = context;
-		}
-
 		[HttpGet]
 		public async Task<ActionResult<List<Activity>>> GetActivities()
 		{
-			return await _context.Activities.ToListAsync();
+			return await Mediator.Send(new List.Query());
 		}
 
 		[HttpGet("{id}")]
 		public async Task<ActionResult<Activity>> GetActivity(Guid id)
 		{
-			return await _context.Activities.FindAsync(id);
+			return await Mediator.Send(new Details.Query { Id = id });
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateActivity(Activity activity)
+		{
+			await Mediator.Send(new Create.Command { Activity = activity });
+			return Ok();
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+		{
+			activity.Id = id;
+			await Mediator.Send(new Update.Command { Activity = activity });
+			return Ok();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteActivity(Guid id)
+		{
+			await Mediator.Send(new Delete.Command { Id = id});
+			return Ok();
 		}
 	}
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistance;
 using Persistence;
@@ -7,13 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +20,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
