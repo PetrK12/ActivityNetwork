97a280e32310fbee785f2df1a0541cc2cf189cf9
Petrk12 <petrkutil123@gmail.com>
2023-12-23 18:59:39 +0100

    Server side validation & exception middleware

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Application.Activities;
 using Application.Core;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Persistance;
 
@@ -26,6 +28,9 @@ namespace API.Extensions
 
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<Create>();
+
             return services;
         }
 	}
--- Application/Activities/Update.cs
+++ Application/Activities/Update.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Application.Core;
 using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistance;
 
@@ -8,12 +10,20 @@ namespace Application.Activities
 {
 	public class Update
 	{
-		public class Command : IRequest
+		public class Command : IRequest<Result<Unit>>
 		{
 			public Activity Activity { get; set; }
 		}
 
-        public class Handler : IRequestHandler<Command>
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -24,13 +34,22 @@ namespace Application.Activities
                 _mapper = mapper;
             }
 
-            public async Task Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
+                if (activity == null) return null;
+
                 _mapper.Map(request.Activity, activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (!result)
+                {
+                    return Result<Unit>.Failure("Activity was not updated");
+                }
+
+                return Result<Unit>.Succes(Unit.Value);
             }
         }
     }
