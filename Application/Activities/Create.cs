1873a1606c34e624b73537d38ea1434cd5d453a3
Petrk12 <petrkutil123@gmail.com>
2023-12-28 22:52:08 +0100

    Added infrastructure project, added attendees relation

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistance;
 
 namespace Application.Activities
@@ -25,19 +27,35 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _context = context;
+                _userAccessor = userAccessor;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users.FirstOrDefaultAsync(
+                    x => x.UserName == _userAccessor.GetUserName());
+
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+
+                request.Activity.Attendees.Add(attendee);
+
                 _context.Activities.Add(request.Activity);
 
                 var result = await _context.SaveChangesAsync() > 0;
 
-                return result ? Result<Unit>.Failure("Failed to create activity") : Result<Unit>.Succes(Unit.Value);
+                if (!result) return Result<Unit>.Failure("Failed to create activity");
+
+                return Result<Unit>.Succes(Unit.Value);
             }
         }
     }
---
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace Infrastructure.Security
+{
+	public class IsHostRequirement : IAuthorizationRequirement
+	{
+	}
+
+    public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
+    {
+        private readonly DataContext _dbContext;
+        private readonly IHttpContextAccessor _httpContextAccesor;
+
+        public IsHostRequirementHandler(IHttpContextAccessor httpContextAccesor, DataContext dbContext)
+        {
+            _httpContextAccesor = httpContextAccesor;
+            _dbContext = dbContext;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            var activityId = Guid.Parse(_httpContextAccesor.HttpContext?
+                .Request.RouteValues.SingleOrDefault(
+                x => x.Key == "id").Value?.ToString());
+
+            var atendee = _dbContext.ActivityAttendees
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.AppUserId == userId
+                    && x.ActivityId == activityId)
+                .Result;
+
+            if (atendee == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (atendee.IsHost)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
+
